53fe17a3c2094107ccaa853575bbd0f88456b16a
Nicchorff <pedrohkerkoff123@gmail.com>
2024-04-30 15:35:49 -0300

    feat: Mostrar quem foi selecionado

--- src/edu/pedro/estruturacondicional/SwitchCase.java
+++ src/edu/pedro/estruturacondicional/SwitchCase.java
@@ -1,26 +1,45 @@
 package edu.pedro.estruturacondicional;
-import java.util.Scanner;
+import java.util.*;
 
 public class SwitchCase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        /*int numero = 3;
 
-        switch (numero){
-            case 1:{
-                System.out.println(numero);
-                break;
+        selecaoCandidato();
+
+    }
+
+    static void selecaoCandidato(){
+        String [] candidatos = {"Pedro", "Lais", "Victor", "Cristina", "Harry", "Salua", "Leticia", "Miguel"};
+        List<String> selecionados = new ArrayList<>();
+        var candidatosSelecionados = 0;
+        var candidatoAtual = 0;
+        var salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            var salarioCandidato = gerarSalario();
+
+            System.out.println("O candidato " + candidato + " solicitou esse valor de salario " + salarioCandidato);
+            if(salarioBase >= salarioCandidato){
+                candidatosSelecionados++;
+                selecionados.add(candidato);
             }
-            case 2:{
-                System.out.println("Selecionou o numero DOIS");
-                break;
-            }
-            default:
-                System.out.println("Undefined");
+            candidatoAtual++;
+        }
+        System.out.println("Total de candidatos Selecionados: " + candidatosSelecionados);
+        infoCandidatoSelecionado(selecionados);
+    }
 
-        }*/
-        analisarCandidato(sc.nextDouble());
+    static void infoCandidatoSelecionado(List<String> canditatosSelecionados){
+        for(var i = 0; i < canditatosSelecionados.size(); i++){
+            System.out.println(canditatosSelecionados.get(i) + " foi selecionado");
+        }
+    }
 
+    static double gerarSalario(){
+        Random rdm = new Random();
+        return rdm.nextDouble(1800,2200);
     }
 
     static void analisarCandidato(double salarioPretendido){
